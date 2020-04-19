5fb919ebe0b3acb8af41bd60e4e4e30159e29827
akshay <akshay.dr.11@gmail.com>
2020-04-19 18:46:04 +0530

    Added New object instead of cube with 22 vertices

--- homework1/homework1.js
+++ homework1/homework1.js
@@ -3,9 +3,9 @@
 var canvas;
 var gl;
 
-var numVertices  = 36;
+var numVertices  = 132;
 
-var numChecks = 8;
+var numChecks = 24;
 
 var c;
 
@@ -26,14 +26,39 @@ var modelViewMatrixLoc;
 var modelViewMatrix;
 
 var vertices = [
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5, -0.5, -0.5, 1.0 )
+    vec4(-0.75, -0.45, -0.45, 1.0), //0
+    vec4(-0.75, -0.45, 0.45, 1.0),  //1
+    vec4(-0.75, 0.45, -0.45, 1.0),  //2
+    vec4(-0.75, 0.45, 0.45, 1.0),   //3
+    vec4(-0.45, -0.75, -0.45, 1.0), //4
+    vec4(-0.45, -0.75, 0.45, 1.0),  //5
+    vec4(-0.45, -0.45, -0.75, 1.0), //6
+    vec4(-0.45, -0.45, 0.75, 1.0),  //7
+    vec4(-0.45, 0.45, -0.75, 1.0),  //8
+    vec4(-0.45, 0.45, 0.75, 1.0),   //9
+    vec4(-0.45, 0.75, -0.45, 1.0),  //10
+    vec4(-0.45, 0.75, 0.45, 1.0),   //11
+    vec4(0.45, -0.75, -0.45, 1.0),  //12
+    vec4(0.45, -0.75, 0.45, 1.0),   //13
+    vec4(0.45, -0.45, -0.75, 1.0),  //14
+    vec4(0.45, -0.45, 0.75, 1.0),   //45
+    vec4(0.45, 0.45, -0.75, 1.0),   //16
+    vec4(0.45, 0.45, 0.75, 1.0),    //17
+    vec4(0.45, 0.75, -0.45, 1.0),   //18
+    vec4(0.45, 0.75, 0.45, 1.0),    //19
+    vec4(0.75, -0.45, -0.45, 1.0),  //20
+    vec4(0.75, -0.45, 0.45, 1.0),   //21
+    vec4(0.75, 0.45, -0.45, 1.0),   //22
+    vec4(0.75, 0.45, 0.45, 1.0)     //23
+
+    // vec4( -0.5, -0.5,  0.5, 1.0 ),
+    // vec4( -0.5,  0.5,  0.5, 1.0 ),
+    // vec4( 0.5,  0.5,  0.5, 1.0 ),
+    // vec4( 0.5, -0.5,  0.5, 1.0 ),
+    // vec4( -0.5, -0.5, -0.5, 1.0 ),
+    // vec4( -0.5,  0.5, -0.5, 1.0 ),
+    // vec4( 0.5,  0.5, -0.5, 1.0 ),
+    // vec4( 0.5, -0.5, -0.5, 1.0 )
 ];
 
 var vertexColors = [
@@ -44,7 +69,26 @@ var vertexColors = [
     vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
     vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
     vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
+    vec4( 0.0, 1.0, 1.0, 1.0 ),   // cyan
+
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
+    vec4( 0.0, 1.0, 1.0, 1.0 ),   // cyan
+
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+
 ];
 
 function quad(a, b, c, d) {
@@ -56,37 +100,91 @@ function quad(a, b, c, d) {
 
      pointsArray.push(vertices[a]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[4]);
 
      pointsArray.push(vertices[b]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[4]);
 
      pointsArray.push(vertices[c]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[4]);
 
      pointsArray.push(vertices[a]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[4]);
 
      pointsArray.push(vertices[c]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[4]);
 
      pointsArray.push(vertices[d]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[a]);
+     colorsArray.push(vertexColors[4]);
+}
+
+function tri(a, b, c) {
+
+    var t1 = subtract(vertices[b], vertices[a]);
+    var t2 = subtract(vertices[c], vertices[b]);
+    var normal = cross(t1, t2);
+    normal = vec3(normal);
+
+     pointsArray.push(vertices[a]);
+     normalsArray.push(normal);
+     colorsArray.push(vertexColors[1]);
+
+     pointsArray.push(vertices[b]);
+     normalsArray.push(normal);
+     colorsArray.push(vertexColors[1]);
+
+     pointsArray.push(vertices[c]);
+     normalsArray.push(normal);
+     colorsArray.push(vertexColors[1]);
 }
 
 function colorCube()
 {
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+    // Sides
+    quad( 0, 1, 3, 2 );
+    quad( 0, 1, 5, 4 );
+    quad( 4, 5, 13, 12 );
+    quad( 12, 13, 21, 20 );
+    quad( 20, 21, 23, 22 );
+    quad( 22, 23, 19, 18 );
+    quad( 18, 19, 11, 10 );
+    quad( 10, 11, 3, 2 );
+
+    // Top and Bottom
+    quad( 17, 15, 7, 9 );
+    quad( 16, 8, 6, 14 );
+
+    // Side Up
+    quad( 19, 17, 9, 11 );
+    tri( 11, 9, 3 );
+    quad( 3, 9, 7, 1 );
+    tri( 1, 7, 5 );
+    quad( 5, 7, 15, 13 );
+    tri( 13, 15, 21 );
+    quad( 21, 15, 17, 23 );
+    tri( 23, 17, 19 );
+
+    // Side bottom
+    quad( 16, 18, 10, 8 );
+    tri( 8, 10, 2 );
+    quad( 8, 2, 0, 6 );
+    tri( 6, 0, 4 );
+    quad( 6, 4, 12, 14 );
+    tri( 14, 12, 20 );
+    quad( 14, 20, 22, 16 );
+    tri( 16, 22, 18 );
+
+    // quad( 1, 0, 3, 2 );
+    // quad( 2, 3, 7, 6 );
+    // quad( 3, 0, 4, 7 );
+    // quad( 6, 5, 1, 2 );
+    // quad( 4, 5, 6, 7 );
+    // quad( 5, 4, 0, 1 );
 }
 
 
@@ -138,9 +236,9 @@ window.onload = function init() {
 }
 
 var render = function() {
-    
+
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
+
     theta[axis] += 2.0;
 
     modelViewMatrix = mat4();
@@ -151,8 +249,8 @@ var render = function() {
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
 
     gl.drawArrays( gl.TRIANGLES, 0, numVertices );
-    
-    // Added delay 
-    setTimeout(function(){ requestAnimationFrame(render); }, 50 );
+
+    // Added delay
+    setTimeout(function(){ requestAnimationFrame(render); }, 30 );
     // requestAnimationFrame(render);
 }
