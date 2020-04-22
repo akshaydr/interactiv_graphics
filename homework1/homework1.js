42fc68b7d837372e14f75f549313c8181916292a
akshay <akshay.dr.11@gmail.com>
2020-04-22 18:30:22 +0530

    Exercise 2 complete with viewer and perspective postion along with sliders

--- homework1/homework1.js
+++ homework1/homework1.js
@@ -10,17 +10,25 @@ var numChecks = 24;
 var c;
 
 var flag = true;
+var thetaflag = true;
+var phiflag = true;
 
-var near = -1;
-var far = 1;
-var left = -1.0;
-var topp = 1.0;
-var right = 1.0;
-var bottom = -1.0;
+var near = 0.3;
+var far = 3.0;
+var radius = 4.0;
+var theta = 0.0;
+var phi = 0.0;
+var dr = 5.0 * Math.PI/180.0;
 
 var pointsArray = [];
 var colorsArray = [];
 var normalsArray = [];
+
+var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
+var  aspect = 1.0;       // Viewport aspect ratio
+var eye;
+const at = vec3(0.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
 
 var xAxis = 0;
 var yAxis = 1;
@@ -28,6 +36,8 @@ var zAxis = 2;
 var axis = 0;
 var theta = [0, 0, 0];
 var thetaLoc;
+
+var angle = 0.0;
 
 var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
@@ -77,28 +87,9 @@ var vertexColors = [
     vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
     vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
     vec4( 0.0, 1.0, 1.0, 1.0 ),   // cyan
-
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
-    vec4( 0.0, 1.0, 1.0, 1.0 ),   // cyan
-
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
-    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
-
 ];
 
-function quad(a, b, c, d) {
+function quad(a, b, c, d, e) {
 
     var t1 = subtract(vertices[b], vertices[a]);
     var t2 = subtract(vertices[c], vertices[b]);
@@ -107,30 +98,30 @@ function quad(a, b, c, d) {
 
      pointsArray.push(vertices[a]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[4]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[b]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[4]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[c]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[4]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[a]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[4]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[c]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[4]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[d]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[4]);
+     colorsArray.push(vertexColors[e]);
 }
 
-function tri(a, b, c) {
+function tri(a, b, c, e) {
 
     var t1 = subtract(vertices[b], vertices[a]);
     var t2 = subtract(vertices[c], vertices[b]);
@@ -139,52 +130,52 @@ function tri(a, b, c) {
 
      pointsArray.push(vertices[a]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[1]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[b]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[1]);
+     colorsArray.push(vertexColors[e]);
 
      pointsArray.push(vertices[c]);
      normalsArray.push(normal);
-     colorsArray.push(vertexColors[1]);
+     colorsArray.push(vertexColors[e]);
 }
 
 function colorCube()
 {
     // Sides
-    quad( 0, 1, 3, 2 );
-    quad( 0, 1, 5, 4 );
-    quad( 4, 5, 13, 12 );
-    quad( 12, 13, 21, 20 );
-    quad( 20, 21, 23, 22 );
-    quad( 22, 23, 19, 18 );
-    quad( 18, 19, 11, 10 );
-    quad( 10, 11, 3, 2 );
+    quad( 0, 1, 3, 2, 0 );
+    quad( 0, 1, 5, 4, 1 );
+    quad( 4, 5, 13, 12, 2 );
+    quad( 12, 13, 21, 20, 3 );
+    quad( 20, 21, 23, 22, 4 );
+    quad( 22, 23, 19, 18, 5 );
+    quad( 18, 19, 11, 10, 6 );
+    quad( 10, 11, 3, 2, 7 );
 
     // Top and Bottom
-    quad( 17, 15, 7, 9 );
-    quad( 16, 8, 6, 14 );
+    quad( 17, 15, 7, 9, 1 );
+    quad( 16, 8, 6, 14, 0 );
 
     // Side Up
-    quad( 19, 17, 9, 11 );
-    tri( 11, 9, 3 );
-    quad( 3, 9, 7, 1 );
-    tri( 1, 7, 5 );
-    quad( 5, 7, 15, 13 );
-    tri( 13, 15, 21 );
-    quad( 21, 15, 17, 23 );
-    tri( 23, 17, 19 );
+    quad( 19, 17, 9, 11, 2 );
+    tri( 11, 9, 3, 3 );
+    quad( 3, 9, 7, 1, 4 );
+    tri( 1, 7, 5, 5 );
+    quad( 5, 7, 15, 13, 5 );
+    tri( 13, 15, 21, 6 );
+    quad( 21, 15, 17, 23, 7 );
+    tri( 23, 17, 19, 0 );
 
     // Side bottom
-    quad( 16, 18, 10, 8 );
-    tri( 8, 10, 2 );
-    quad( 8, 2, 0, 6 );
-    tri( 6, 0, 4 );
-    quad( 6, 4, 12, 14 );
-    tri( 14, 12, 20 );
-    quad( 14, 20, 22, 16 );
-    tri( 16, 22, 18 );
+    quad( 16, 18, 10, 8, 1 );
+    tri( 8, 10, 2, 2 );
+    quad( 8, 2, 0, 6, 3 );
+    tri( 6, 0, 4, 4 );
+    quad( 6, 4, 12, 14, 5 );
+    tri( 14, 12, 20, 6 );
+    quad( 14, 20, 22, 16, 7 );
+    tri( 16, 22, 18, 0 );
 
     // quad( 1, 0, 3, 2 );
     // quad( 2, 3, 7, 6 );
@@ -236,10 +227,17 @@ window.onload = function init() {
     projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
 
     //event listeners for buttons
-    document.getElementById("xButton").onclick = function(){axis = xAxis;};
-    document.getElementById("yButton").onclick = function(){axis = yAxis;};
-    document.getElementById("zButton").onclick = function(){axis = zAxis;};
-    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+    // document.getElementById("xButton").onclick = function(){axis = xAxis;};
+    // document.getElementById("yButton").onclick = function(){axis = yAxis;};
+    // document.getElementById("zButton").onclick = function(){axis = zAxis;};
+    // document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+
+    // sliders for viewing parameters
+    document.getElementById("zNearSlider").onchange=function(event){near = event.target.value;};
+    document.getElementById("radiusSlider").onchange=function(event){radius = event.target.value;};
+    document.getElementById("thetaSlider").onchange=function(event){theta = event.target.value* Math.PI/180.0;};
+    document.getElementById("phiSlider").onchange=function(event){phi = event.target.value* Math.PI/180.0;};
+    document.getElementById("fovSlider").onchange=function(event){fovy = event.target.value;};
 
     render();
 }
@@ -248,14 +246,18 @@ var render = function() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    if(flag) theta[axis] += 2.0;
+    // if(flag) theta[axis] += 2.0;
 
-    modelViewMatrix = mat4();
-    modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], vec3(1, 0, 0)));
-    modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], vec3(0, 1, 0)));
-    modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], vec3(0, 0, 1)));
-   
-    projectionMatrix = ortho(left, right, bottom, topp, near, far);
+    // modelViewMatrix = mat4();
+    // modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], vec3(1, 0, 0)));
+    // modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], vec3(0, 1, 0)));
+    // modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], vec3(0, 0, 1)));
+
+    eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
+        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+
+    modelViewMatrix = lookAt(eye, at, up);
+    projectionMatrix = perspective(fovy, 1, near, far);
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
